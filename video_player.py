cf788e38032cd8249d17ee0f6573a4a222df0980
mengxiang <2547881370.com>
2024-04-27 01:10:43 +0800

    1

--- video_player.py
+++ video_player.py
@@ -5,6 +5,7 @@ import cv2
 import pygame
 import socket
 import os
+from flask import Flask, request, jsonify
 
 class VideoPlayer:
     def __init__(self):
@@ -14,6 +15,19 @@ class VideoPlayer:
         self.path = {}
 
     def add_video(self, video_path, audio_path):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frames.append(frame)
+        cap.release()
+        self.video_frames[audio_path] = frames
+        self.audio_paths.append(audio_path)
+        self.path[audio_path] = video_path
+
+    def preload_video(self, video_path, audio_path):
         cap = cv2.VideoCapture(video_path)
         frames = []
         while True:
@@ -56,21 +70,6 @@ class VideoPlayer:
             # os.remove(self.path[audio_path])
         self.playing = False
 
-def read_next_video(file_path, oldest_video):
-        player.add_video(file_path, oldest_video)
-                
-        print("执行",file_path, oldest_video)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                player.playing = False
-                pygame.quit()
-                return
-
-        if not player.playing and player.audio_paths:
-            player.play(screen)
-        
-        
 def read_and_remove_first_object(json_file = 'video_data.json'):
     with open(json_file, 'r') as f:
         data = json.load(f)
@@ -82,19 +81,30 @@ def read_and_remove_first_object(json_file = 'video_data.json'):
             json.dump(data, f, indent=4)
         
         return first_object
-        
+            
+            
 # 测试
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((800, 500))
     player = VideoPlayer()
-    
+
     while True:
         read_result = read_and_remove_first_object()
         if read_result:
-            print("读取到的数据：", read_result.get('file_path'), read_result.get('oldest_video'))
-            read_next_video(read_result.get('file_path'),read_result.get('oldest_video'))
-        time.sleep(0.5)
+                player.preload_video(
+                    read_result.get('file_path'),read_result.get('oldest_video')
+                )
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                player.playing = False
+                pygame.quit()
+                break
+
+        if not player.playing and player.audio_paths:
+            player.play(screen)
+    
 
 
     
